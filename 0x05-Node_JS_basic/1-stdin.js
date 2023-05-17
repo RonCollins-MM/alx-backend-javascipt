58c44dc31765de7d7b1f69016504b649f641abb5
RonCollins-MM <muriukiron@gmail.com>
2023-05-17 12:40:41 +0300

    get user input from `STDIN` and display to `STDOUT`

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+/** Simple function that accepts user input from STDIN and prints to STDOUT */
+
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('data', (d) => {
+  if (d.toString().trim() !== '') {
+    process.stdout.write(`Your name is: ${d.toString()}`);
+  } else {
+    process.exit();
+  }
+});
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+  process.exit();
+});
